58d164c617f4412c9bec34b5571f74960d7f4901
麒 王 <leech@qideMacBook-Pro.local>
2015-04-02 17:13:52 +0800

    add doc

---
+++ mac 装机.java
@@ -0,0 +1,10 @@
+mac 装机
+1. mac 环境变量设置
+-----------------------------------------------------------------------------------------------------------------------------
+全局 /etc/profile
+系统级别 /etc/bashrc
+用户级别 :~/.bash_profile
+
+
+.java 环境 。10.10 没有自带的Java环境，需要到oracle上面下载。1.8下载地址：http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
+
